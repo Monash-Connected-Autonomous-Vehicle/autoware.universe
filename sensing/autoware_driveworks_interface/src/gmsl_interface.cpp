bd16d298c886462bd45dc14d5e670d4c7030fdf9
dtonda8 <davidbatonda7@gmail.com>
2023-09-20 15:05:50 +1000

    try out basic publisher and subscriber

--- sensing/autoware_driveworks_interface/src/gmsl_interface.cpp
+++ sensing/autoware_driveworks_interface/src/gmsl_interface.cpp
@@ -1,5 +1,10 @@
 #include <cstdio>
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
+
+
 int main(int argc, char ** argv)
 {
   (void) argc;
@@ -8,3 +13,47 @@ int main(int argc, char ** argv)
   printf("hello world autoware_driveworks_interface package\n");
   return 0;
 }
+
+
+/*
+
+#ifndef GMSL_CAMERA_CORE_H
+#define GMSL_CAMERA_CORE_H
+
+// ROS includes
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/String.h>
+
+namespace gmsl_camera
+{
+class GMSLCameraNode
+{
+public:
+  GMSLCameraNode();
+  ~GMSLCameraNode();
+
+  void run();
+  void g_runSetter(bool);
+  int argc_;
+  char **argv_;
+
+private:
+  // handle
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+
+  bool gTakeScreenshot = false;
+  int gScreenshotCount = 0;
+  uint32_t imageWidth;
+  uint32_t imageHeight;
+
+  // initializer
+  void initForROS();
+  void initForDW();
+};
+}
+#endif
+
+*/
