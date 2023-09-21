92f7bfa0e12a1cd6bfa9c2fcdd9ae111e9664f40
Robin-Chea <rche0051@student.monash.edu>
2023-09-22 02:15:22 +1000

    added msg packages for autoware interface

--- sensing/autoware_driveworks_interface/include/autoware_driveworks_interface/gmsl_interface.hpp
+++ sensing/autoware_driveworks_interface/include/autoware_driveworks_interface/gmsl_interface.hpp
@@ -1,10 +1,10 @@
 #ifndef GMSL_CAMERA_CORE_H
 #define GMSL_CAMERA_CORE_H
 
-// ROS includes
-#include "rclcpp/rclcpp.hpp" 
+// ROS 2 includes
+#include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "std_msgs/msg/string.hpp"  
+#include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/int32.hpp"
 
 namespace gmsl_camera
@@ -21,63 +21,18 @@ public:
   char **argv_;
 
 private:
-  // handle
-  //ros::NodeHandle nh_; // ?
-  //ros::NodeHandle private_nh_; // ?
+  // Handle for ROS 2
+  rclcpp::Node::SharedPtr node_;
 
   bool gTakeScreenshot = false;
   int gScreenshotCount = 0;
   uint32_t imageWidth;
   uint32_t imageHeight;
 
-  // initializer
+  // Initializer for ROS 2
   void initForROS();
   void initForDW();
 };
-}
+} // namespace gmsl_camera
+
 #endif
-
-
-// Autoware ai: 
-/*
-
-#ifndef GMSL_CAMERA_CORE_H
-#define GMSL_CAMERA_CORE_H
-
-// ROS includes
-#include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <std_msgs/Int32.h>
-#include <std_msgs/String.h>
-
-namespace gmsl_camera
-{
-class GMSLCameraNode
-{
-public:
-  GMSLCameraNode();
-  ~GMSLCameraNode();
-
-  void run();
-  void g_runSetter(bool);
-  int argc_;
-  char **argv_;
-
-private:
-  // handle
-  ros::NodeHandle nh_;
-  ros::NodeHandle private_nh_;
-
-  bool gTakeScreenshot = false;
-  int gScreenshotCount = 0;
-  uint32_t imageWidth;
-  uint32_t imageHeight;
-
-  // initializer
-  void initForROS();
-  void initForDW();
-};
-}
-#endif
-
-*/
